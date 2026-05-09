a32577f506664ab846319925e8a5bc6b88fd3244
Alysson Pereira <alyssonempire84@gmail.com>
2026-05-09 12:19:26 -0300

    utilizando chi

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,28 +6,24 @@ import (
 	"net/http"
 
 	"api-golang/config"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		http.Error(w, "método nao aceito", 400)
-		return
-	}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "Hello, World!")
 }
 
 func main() {
+	r := chi.NewRouter()
 	env := config.LoadEnvs(".env")
 	fmt.Printf("DB: %s:%s/%s\n", env.DBHost, env.DBPort, env.DBName)
 
-	http.HandleFunc("/healthz", handler)
+	r.Get("/healthz", handler)
 
-	addr := ":" + env.AppPort
-	fmt.Printf("Servidor rodando na porta %s\n", addr)
-	err := http.ListenAndServe(addr, nil)
-	if err != nil {
-		log.Fatal("erro ao iniciar o servidor:", err)
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		log.Fatal(err)
 	}
 }
